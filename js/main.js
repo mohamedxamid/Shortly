355b025d7c6403cf3554232561efdb212277410a
Muhammad Xamidov <muhammadxamidov001@gmail.com>
2023-12-18 13:49:10 +0300

    Add local storage feature

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ const elUrlShortener = document.querySelector('.url-shortener');
 const elSiteHeader = document.querySelector('.site-header');
 const elSitenavToggler = elSiteHeader.querySelector('.site-header__sitenav-toggler');
 
+let urlList = JSON.parse(localStorage.getItem("urlList")) || [];
+
 if (elSitenavToggler) {
     elSitenavToggler.addEventListener('click', function () {
         elSitenavToggler.classList.toggle(modifiers.sitenavTogglerActive);
@@ -19,14 +21,26 @@ if (elSitenavToggler) {
 if(elUrlShortenerForm) {
     elUrlShortenerForm.addEventListener('submit', function(evt) {
         evt.preventDefault();
-    
+        
+        const formData = new FormData(evt.target)
+        const {url} = Object.fromEntries(formData.entries());
+        urlList.unshift({url, shortUrl: 'https://rel.ink/k4lKyk'});
+        evt.target.reset();
+        if(urlList.length > 3) {
+            urlList.pop()
+        }
+        elUrlShortenerResults.replaceChildren(...renderResultUrl(urlList))
+        localStorage.setItem("urlList", JSON.stringify(urlList))
         elUrlShortenerResults.classList.add('url-shortener__results--open')
+        setTimeout(() => {
+            elUrlShortenerResults.classList.remove('url-shortener__results--open')
+        }, 6000);
     })
 }
 
 if (elUrlShortener) {
     elUrlShortener.addEventListener('click', function(evt) {
-
+        
         if(evt.target.matches('.js-copy-short-url-button')) {
             // Copy text link
             navigator.clipboard.writeText(evt.target.previousElementSibling.textContent);
@@ -36,7 +50,7 @@ if (elUrlShortener) {
             
             // Change bgcolor button
             evt.target.classList.add('url-shortener__copy-button--copied');
-
+            
             // Reset text and class button after 1 second
             setTimeout(function() {
                 evt.target.textContent = 'Copy';
@@ -45,3 +59,16 @@ if (elUrlShortener) {
         };
     });
 };
+
+function renderResultUrl(urlList) {
+    return urlList.map(({url, shortUrl}) => {
+        const div = document.createElement('div')
+        div.classList.add('url-shortener__result')
+        div.innerHTML = `
+        <div class="url-shortener__original-link">${url}</div>
+        <a class="url-shortener__short-link" href="#" target="_blank">${shortUrl}</a>
+        <button class="url-shortener__copy-button button button--small button--square-small button--green js-copy-short-url-button" type="button">Copy</button>
+        `
+        return div;
+    })
+}
